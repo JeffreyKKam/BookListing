b0c35e85863964b4f43c8bc2947423eb49a501f6
jkam <jeff.k.kam@gmail.com>
2019-08-22 20:38:06 -0600

    addng in basic search bar

--- BookListing.Website/ClientApp/src/app/book-list/book-list.component.ts
+++ BookListing.Website/ClientApp/src/app/book-list/book-list.component.ts
@@ -8,16 +8,26 @@ import { HttpClient } from '@angular/common/http';
 export class BookListComponent {
   public results;
   public baseURL;
+  searchValue = '';
+  page = 0;
 
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.baseURL = baseUrl;
-    this.search("", 1);
+    this.search(this.searchValue, this.page);
   }
 
   search(searchTerm, page) {
-    this.http.get(this.baseURL + 'api/Book/Search').subscribe(result => {
+
+    this.http.get(this.baseURL + 'api/Book/Search', {
+      params: { searchTerm: searchTerm, page : page }
+    }).subscribe(result => {
       this.results = result;
     }, error => console.error(error));
+  }
+
+  // possibly used to get suggestions
+  onKey(value: string) {
+    this.searchValue = value;
   }
 }
 
